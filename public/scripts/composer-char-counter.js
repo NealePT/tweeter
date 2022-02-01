653ee6023927b8d7cd0d2d1594dfb4009fe051ab
Neale Taylor <nealeptaylor@gmail.com>
2022-02-01 14:46:12 -0500

    ipmlement counter logic and add conditional for negative numbers

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
   $('#tweet-text').on('input', function(characterCounterCallback) {
     let charCount = this.value.length;
     let charRemaining = (140 - charCount);
-    console.log(charRemaining);
+    // console.log(charRemaining);
+
+    let counter = $(this).parent().next('section').children('.counter');
+    counter.text(charRemaining);
+
+    if (charRemaining < 0) {
+      counter.addClass('negativeCount');
+    } else {
+      counter.removeClass('negativeCount');
+    }
   });
 });
