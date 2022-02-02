ff8c06d1ae52eb2c21cfade286a4cd0871197383
Neale Taylor <nealeptaylor@gmail.com>
2022-02-02 13:54:34 -0500

    implement dynamic tweets and remove tweet layout from index.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,66 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1643738754428
+  }
+  ]
+
+  const createTweetElement = function(data) {
+    const layout = `
+    <article class="tweet">
+      <header class="tweetHeader">
+        <div class="headerLeft">
+          <img src="${data.user.avatars}" alt="">
+          <p class="tweetAuthorName">${data.user.name}</p>
+        </div>
+        <div class="headerRight">
+          <h4 class="tweetAuthorHandle">${data.user.handle}</h4>
+        </div>
+      </header>
+      <section class="tweetMain">
+        <p>${data.content.text}</p>
+      </section>
+      <footer class="tweetFooter">
+        <p>${data.created_at}</p>
+        <div class="footerIcons">
+          <i id="flag" class="fas fa-flag"></i>
+          <i id="retweet" class="fas fa-retweet"></i>
+          <i id="heart" class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+    `
+    $('#tweets-container').append(layout)
+  }
+
+  const appendMultipleTweets = function(data) {
+    for (let tweet of data) {
+      createTweetElement(tweet);
+    }
+  }
+  appendMultipleTweets(data);
+});
+
